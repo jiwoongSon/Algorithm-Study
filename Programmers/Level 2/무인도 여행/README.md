a644465d04930629183e5fcef9167fd9a7bc8e5c
jiwoongSon <thswldnd7@naver.com>
2026-04-05 14:00:03 +0900

    무인도 여행

--- Programmers/Level 2/무인도 여행/README.md
+++ Programmers/Level 2/무인도 여행/README.md
@@ -13,6 +13,20 @@
             matrix[i] = maps[i].toCharArray();
         }
 ```
-1차원 배열을 2차원 배열로 ㅂ
-## 개선할 점(코드)
+1차원 배열을 2차원 배열로 배열로 바꾸는 과정에서, for문 중첩보다 빠르다. 
+maps[i].toCharArray(); 이 부분에서 1차원 배열을 생성한다. 그 1차원 배열을 matrix[i]에 할당
+
+```java
+Queue<int[]> que = new LinkedList<>();
+```
+BFS는 가장 가까운 곳부터 공평하게 탐색하는 것이다. 따라서, FIFO 구조를 가져야 한다. Queue는 보통 linkedList, ArrayDeque로 구현. 
+
+여기서의 que, 다익스트라 알고리즘의 priorityQue 등은 탐색 예정인 대기열이다. 이때 다음에 어떤 것을 탐색하는지에 따라(BFS, DFS) 다른 자료구조를 사용하면 된다. 
+
 ## 성능
+O(N * M log(N * M))
+2차원 배열 준비, BFS 탐색 N*M (맵의 크기)
+정렬하는 경우 KlogK 
+    - 내부적으로 Sort는 Timsort 사용
+    - 삽입정렬과 병합정렬의 장점만 사용
+    - 전체 데이터를 Run(작은 덩어리) -> Run 내부는 삽입 정렬 사용 -> Run끼리 merge sort로 정렬
--- Programmers/Level 2/무인도 여행/Solution.java
+++ Programmers/Level 2/무인도 여행/Solution.java
@@ -2,14 +2,42 @@ import java.util.*;
 
 class Solution {
 
-    public int bfs(int StartR, int startC, int n, int m, char[][] matrix, boolean[][] visited) {
-        
-    }
+    public int bfs(int startR, int startC, int n, int m, char[][] matrix, boolean[][] visited) {
+        Queue<int[]> que = new LinkedList<>();
 
         int[] dr = {-1, 1, 0 ,0};
         int[] dc = {0, 0, 1, -1};
 
+        que.offer(new int[]{startR, startC});
+        visited[startR][startC] = true;
+
+        int sum = matrix[startR][startC] -'0';
+
+        while(!que.isEmpty()) {
+            int[] current = que.poll();
+            int r = current[0];
+            int c = current[1];
+            
+            for(int i = 0; i < 4; i++) {
+                int nr = r + dr[i];
+                int nc = c + dc[i];
+
+                if(nr < 0 || nr >= n || nc < 0 || nc >= m) continue;
+                if(matrix[nr][nc] == 'X') continue;
+                if(visited[nr][nc]) continue;
+
+                visited[nr][nc] = true; 
+                que.offer(new int[]{nr, nc}); 
+                
+                sum += matrix[nr][nc] - '0'; 
+            }
+        }
+        return sum;
+    }
+
+
     public int[] solution(String[] maps) {
+
         int n = maps.length;
         int m = maps[0].length();
         char[][] matrix = new char[n][m];
@@ -28,12 +56,23 @@ class Solution {
         for(int i = 0; i<n; i++) {
             for(int j = 0; j<m; j++) {
                 if(matrix[i][j] != 'X' && !visited[i][j]) {
-
+                    int sum = bfs(i,j,n,m,matrix,visited);
+                    land.add(sum);
                 }
             }
         }
 
+        if (land.isEmpty()) {
+            return new int[]{-1};
+        }
 
+        Collections.sort(land); // 오름차순 정렬
+
+        int[] answer = new int[land.size()];
+        for (int i = 0; i < land.size(); i++) {
+            answer[i] = land.get(i);
+        }
+        return answer;
     }
 }
 
